3a6baa68a128495a269fbd8f1587fd769d408322
Kamael1981 <rengkahkamael@gmail.com>
2025-08-15 12:52:52 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1>Hello Vercel!</h1>
+  </React.StrictMode>
+);
