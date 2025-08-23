ee24323b34e50844c61bbec97316d7fa5029c775
Kamael1981 <rengkahkamael@gmail.com>
2025-08-23 09:11:31 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element with id 'root' not found");
+
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <h1>Hello Vercel!</h1>
   </React.StrictMode>
